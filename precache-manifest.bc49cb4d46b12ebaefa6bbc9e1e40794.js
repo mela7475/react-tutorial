849106b760577ab4a4f20142e4361aeab7d533ec
Jordan Melancon <42441310+mela7475@users.noreply.github.com>
2019-10-09 15:11:33 -0500

    Updates

---
+++ precache-manifest.bc49cb4d46b12ebaefa6bbc9e1e40794.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29bb1819a279bb3b7ea83de465a7b111",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "68c97460a5bae8ca7215",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "cf427a06b3ed50db74ab",
+    "url": "/react-tutorial/static/js/2.1495c55b.chunk.js"
+  },
+  {
+    "revision": "68c97460a5bae8ca7215",
+    "url": "/react-tutorial/static/js/main.6003f609.chunk.js"
+  },
+  {
+    "revision": "829259c153935269c4b9",
+    "url": "/react-tutorial/static/js/runtime-main.29f4f767.js"
+  }
+]);
